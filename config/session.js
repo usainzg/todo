8f0dd4387bfc65c9548c7f54490df8cf4d19f034
Unai Sainz <unaihtc70@gmail.com>
2015-11-11 20:53:31 +0100

    First commit

--- config/session.js
+++ config/session.js
@@ -41,7 +41,7 @@ module.exports.session = {
   * session store that can be shared across multiple Sails.js servers        *
   ***************************************************************************/
 
-  //adapter: 'redis',
+  adapter: 'redis',
 
   /***************************************************************************
   *                                                                          *
@@ -52,12 +52,12 @@ module.exports.session = {
   *                                                                          *
   ***************************************************************************/
 
-  // host: 'localhost',
-  // port: 6379,
-  // ttl: 24 * 60 * 60,
-  // db: 0,
-  // //pass: <redis auth password>,
-  // prefix: 'sess:',
+  host: 'localhost',
+  port: 6379,
+  ttl: 24 * 60 * 60,
+  db: 0,
+  //pass: <redis auth password>,
+  prefix: 'sess:',
 
 
   /***************************************************************************

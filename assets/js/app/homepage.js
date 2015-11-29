4f0bb0f1ebe0a3ec9a4d879b3c8f13077174ed14
Joseba Legarreta <josebalega@gmail.com>
2015-11-29 18:22:04 +0100

    Fixed collapsible ul

--- assets/js/app/homepage.js
+++ assets/js/app/homepage.js
@@ -1,5 +1,18 @@
-$(document).ready(function(){
-    $('.collapsible').collapsible({
-      accordion : false
-    });
-  });
+$(function(){
+
+  // FIX: Collapsible elements only need initialization if they are added dynamically.
+  // You can also pass in options inside the initialization, however this can be done in the HTML markup as well.
+
+  // Explanation, we are not adding them dinamically, we're adding them as static HTML
+  // If we had create them via JS, dinamically, we would need the following code
+
+  // $('.collapsible').collapsible({
+  //   accordion : false
+  // });
+
+  // But in our case we don't need this code, this code cancels the collapsible action
+  // Just dropping it it's fixed your problem!!
+
+  // Tomorrow will see it in more detail
+
+});
